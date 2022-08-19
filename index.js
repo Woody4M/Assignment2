22d616951605e5eda59c53653f328dca141f0d9e
daniel otanez <111538726+Woody4M@users.noreply.github.com>
2022-08-19 01:56:57 +0000

    samemm

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 
 
-let myhttp= require("http");
+let myhttp = require("http");
 myhttp.createServer(
-  function( myrequest, myresponse ){
-    console.log( myrequest.url);
+  function(myrequest, myresponse) {
+    console.log(myrequest.url);
     let mytext;
-    if(myreuqest.url==="/hey"){
-      let mytext="Well hello there...";
+    if (myreuqest.url === "/hey") {
+      let mytext = "Well hello there...";
     }
-    else{
-      let mytext="i dont know you!";
-      
+    else {
+      let mytext = "i dont know you!";
+
     }
-    myresponse.writeHead( 200, {"Content-type": "text/plain"});
+    myresponse.writeHead(200, { "Content-type": "text/plain" });
     myresponse.end("hello Everyone!\n");
   }
 );
- myserver.listen(8080,"0.0.0.0");
+myserver.listen(8080, "0.0.0.0");
 console.log("server has started");
